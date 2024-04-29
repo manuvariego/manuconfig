2a0c7546b34c55cb662d264efd0fe30797d25cc4
Manuel Variego <73005152+manuvariego@users.noreply.github.com>
2024-04-29 15:59:44 -0300

    Remodeled config

---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+--
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
