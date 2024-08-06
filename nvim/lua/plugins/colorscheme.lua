a87807d205cf5956c15964d49432e3f4b9030a79
manuvariego <73005152+manuvariego@users.noreply.github.com>
2024-08-05 22:51:50 -0300

    add: everforest colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,30 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function ()
-      require("tokyonight").setup({
-        style = "storm"
-
-
-      })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    {
+      "neanias/everforest-nvim",
+      version = false,
+      lazy = false,
+      priority = 1000, -- make sure to load this before all the other start plugins
+      -- Optional; default configuration will be used if setup isn't called.
+      config = function()
+        require("everforest").setup({
+          -- Your config here
+        })
+        vim.cmd([[colorscheme everforest]])
+      end,
+    },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function ()
+  --     require("tokyonight").setup({
+  --       style = "storm"
+  --
+  --
+  --     })
+  --     vim.cmd([[colorscheme tokyonight]])
+  --   end,
+  -- },
 
 }
