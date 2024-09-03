a67d40e07d6c7c82173aeedc6e5b233ea14f00d2
Manuel Variego <73005152+manuvariego@users.noreply.github.com>
2024-09-02 22:07:53 -0300

    add: conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+
+    'stevearc/conform.nvim',
+    config = function()
+        require('conform').setup({
+            formatters_by_ft = {
+                go = { "gofmt", "goimports" },
+
+                typescript = { "ts-standard" },
+                python = { "pyright" },
+
+            },
+
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+
+            },
+        })
+    end,
+
+
+
+
+}
