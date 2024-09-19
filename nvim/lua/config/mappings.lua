51ecd3fcfdd20a3ba142e675cf6b2daebc4f210a
manuvariego <73005152+manuvariego@users.noreply.github.com>
2024-09-19 18:03:29 -0300

    change: remove harpoon telescope

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 --Harpoon mappings
 local harpoon = require("harpoon")
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add to harpoon list" })
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Select harpoon 1" })
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Select harpoon 2" })
